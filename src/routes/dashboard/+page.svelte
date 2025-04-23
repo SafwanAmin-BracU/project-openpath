0786d3badae1af42541b6e99abf09c055da309d6
Safawn Amin <123196288+SafwanAmin-BracU@users.noreply.github.com>
2025-04-23 07:10:16 +0600

    Adds alt text and removes auto adapter config
    Improves accessibility by adding alt text to under construction image
    Removes unused auto adapter configuration from Svelte config to simplify setup

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -4,6 +4,6 @@
         <p class="mt-4 text-lg">This is the main page for the web app.</p>
     </div>
     <div class="flex flex-col items-center justify-center w-full h-fit">
-        <img src="https://static.vecteezy.com/system/resources/previews/001/218/694/non_2x/under-construction-warning-sign-vector.jpg" class="w-1/2 h-auto " />
+        <img src="https://static.vecteezy.com/system/resources/previews/001/218/694/non_2x/under-construction-warning-sign-vector.jpg" alt="Under Construction" class="w-1/2 h-auto " />
 </div>
 </div>

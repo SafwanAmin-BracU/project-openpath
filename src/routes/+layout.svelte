eec5bde2a692b119d5aff3584a68cab69df98357
Safawn Amin <123196288+SafwanAmin-BracU@users.noreply.github.com>
2025-04-23 14:27:17 +0600

    refactor: update page layout and styling
    - Simplify layout structure in +layout.svelte by removing redundant navigation links
    - Enhance intro page design in +page.svelte with new flex and rounded elements
    - Add custom color variables and update body background in app.tailwind.css

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,9 @@
 <script lang="ts">
-	import '$lib/app.tailwind.css';
-	
+	import "$lib/app.tailwind.css";
+
 	let { children } = $props();
 </script>
-<div class="flex flex-row justify-center w-full gap-3 h-fit">
-	<a href="/">Home</a>
-	<a href="/auth">Auth </a>
-	<a href="/dashboard">Dashboard</a>
-</div>
-<div class="flex flex-col w-full h-full">
 
+<div class="flex flex-col w-full h-full">
 	{@render children()}
 </div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,6 @@
 <div class="flex items-center justify-center w-full h-full">
-    This is the intro page.
+  <div class="flex h-fit w-fit overflow-clip rounded-3xl">
+    <div class="h-[36rem] w-[20rem] bg-[var(--color-primary)]"></div>
+    <div class="h-[36rem] w-[20rem] bg-white"></div>
+  </div>
 </div>

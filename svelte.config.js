85d465192b09a51ba2d29400dffe2f7f787a8090
Safawn Amin <123196288+SafwanAmin-BracU@users.noreply.github.com>
2025-04-24 09:26:35 +0600

    build(svelte): add Cloudflare adapter to svelte.config.js
    This commit adds the Cloudflare adapter to the Svelte configuration file to enable deployment on Cloudflare's platform. The adapter is imported and configured within the `kit` section of the config.

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,14 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import adapter from "@sveltejs/adapter-cloudflare";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
-
+  // Consult https://svelte.dev/docs/kit/integrations
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
+  kit: {
+    adapter: adapter(),
+  },
 };
 
 export default config;

b05d315e278077d0284e3c7843c5faf5d508223a
Safawn Amin <123196288+SafwanAmin-BracU@users.noreply.github.com>
2025-04-29 15:55:09 +0600

    refactor(dashboard): remove sidebar layout to simplify UI
    The sidebar layout was removed to streamline the dashboard interface and reduce unnecessary complexity. This change focuses on improving the user experience by making the layout cleaner and more straightforward.

--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -2,37 +2,8 @@
   let { children } = $props();
 </script>
 
-<div class="flex flex-row w-full h-full">
-  <div class="flex max-w-[240px] h-full bg-slate-200 min-w-fit">
-    <div class="flex flex-col gap-2 p-4">
-      <div class="flex flex-col gap-2 justify-center">
-        <a
-          href="/"
-          class="text-slate-600 font-medium"
-        >
-          <span class="icon-[mdi--home] size-6"></span>
-          <span>Home</span>
-        </a>
-        <a
-          href="/projects"
-          class="text-slate-600 font-medium"
-        >
-          <span class="icon-[mdi--code-tags] size-6"></span>
-          <span>Projects</span>
-        </a>
-        <a
-          href="/dashboard"
-          class="text-slate-600 font-medium"
-        >
-          <span class="icon-[mdi--account-multiple] size-6"></span>
-          <span>Dashboard</span>
-        </a>
-      </div>
-    </div>
-  </div>
-  <div class="flex w-full h-full bg-slate-100">
-    {@render children()}
-  </div>
+<div class="flex w-full h-full bg-slate-100">
+  {@render children()}
 </div>
 
 <style lang="postcss">
